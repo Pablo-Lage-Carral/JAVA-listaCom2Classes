8493fa8b61a1b9fa17db0ba83671c2b95e85c5db
Pablo Lage Carral <124844088+Pablo-Lage-Carral@users.noreply.github.com>
2023-03-03 21:21:31 -0300

    Create Endereco.java

---
+++ src/br/com/fiap/beans/Endereco.java
@@ -0,0 +1,54 @@
+package br.com.fiap.beans;
+
+public class Endereco {
+	
+	private String logradouro;
+	private int numero;
+	private String bairro;
+	private String estado;
+	
+
+	public Endereco() {
+		super();
+	}
+	
+	
+	
+	public Endereco(String logradouro, int numero, String bairro, String estado) {
+		super();
+		this.logradouro = logradouro;
+		this.numero = numero;
+		this.bairro = bairro;
+		this.estado = estado;
+	}
+
+
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
+	
+}
